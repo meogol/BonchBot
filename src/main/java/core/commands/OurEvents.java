80ced1c049a2e9bf69a1c21cd9c5f0ac5fe7be96
Sparrow14 <kirill1407@yandex.ru>
2021-05-03 14:41:53 +0300

    Add new events to feedback

---
+++ src/main/java/core/commands/OurEvents.java
@@ -0,0 +1,23 @@
+package core.commands;
+
+import com.vk.api.sdk.objects.messages.Message;
+import core.Command;
+import vk.VKManager;
+import vk.callback.data.ClientInfo;
+
+public class OurEvents extends Command implements ServiceCommand {
+
+    public OurEvents(String name) {
+        super(name);
+    }
+
+    @Override
+    public void exec(Message message, ClientInfo clientInfo) {
+        new VKManager().sendMessage("Спасибо за подписку!\nВсе новости будут приходить в 19:00!", message.getPeerId());
+    }
+
+    @Override
+    public void service() {
+
+    }
+}
